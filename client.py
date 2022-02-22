baa94a3313effbecb1234b5b8a43212f98898027
Jack Volonte <volonte22@up.edu>
2022-02-22 11:09:40 -0800

    a
    a

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ from network import Network
 width = 1280
 height = 720
 win = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Client")
+pygame.display.set_caption("Untitled Client")
 
 clientNumber = 0
 
@@ -12,11 +12,18 @@ clientNumber = 0
 playerOneCardBackImage = pygame.image.load('resources/backgroundOfCard.jpg')
 background = pygame.image.load('resources/one.jpg')
 
+#text
+pygame.font.init() # you have to call this at the start,
+                   # if you want to use this module.
+myfont = pygame.font.SysFont('Courier', 18)
+
 #initalizing network
 
+#start of the game count
+turnCount = 0
 
 class Player():
-    def __init__(self, x, y, width, height, color):
+    def __init__(self, x, y, width, height, color, turnCount):
         self.x = x
         self.y = y
         self.width = width
@@ -24,9 +31,21 @@ class Player():
         self.color = color
         self.rect = (x, y, width, height) # helps with drawing character
         self.vel = 3
+        self.turnCount = 1
 
     def draw(self, win):
         pygame.draw.rect(win, self.color, self.rect)
+        # end turn button
+        colorA = (107, 130, 73)
+        colorB = (133, 33, 33)
+        if turnCount == 0:
+            pygame.draw.rect(win, (107, 130, 73), (1090, 315, 140, 80))
+        else:
+            pygame.draw.rect(win, (133, 33, 33), (1090, 315, 140, 80))
+
+        pygame.draw.rect(win, (0, 0, 0), (1090, 315, 140, 80), 3)
+        textsurface = myfont.render('End Turn', False, (0, 0, 0))
+        win.blit(textsurface, (1115, 345))
 
     def move(self): # check if player presses button or mouse up and down
         keys = pygame.key.get_pressed()
@@ -43,7 +62,24 @@ class Player():
         if keys[pygame.K_DOWN]:
             self.y += self.vel
 
+        mousePos = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+        endButtonRect = pygame.Rect(1090, 315, 140, 80)
+        if click[0] == 1:
+            if endButtonRect.collidepoint(mousePos):
+                if self.turnCount == 0:
+                    self.turnCount = 1
+                else:
+                    self.turnCount = 0
         self.update()
+
+
+
+
+    def setTurnStart(self):
+        self.turnCount = 1
+    def setTurnSecond(self):
+        self.turnCount = 0
 
     def update(self):
         self.rect = (self.x, self.y, self.width, self.height) #coordinates is top left, so down is adding (top left is 0,0)
@@ -87,28 +123,42 @@ class boardLook():
         self.height = height
         self.cards = cards
 
-    def draw(self, win):
-        # draws both players hands & background of hands
+    def draw(self, win, turnCount):
+        # background of middle board & players hands
         pygame.draw.rect(win, (99, 84, 60), (320, 570, 640, (720-570)))
         pygame.draw.rect(win, (99, 84, 60), (320, 0, 640, 150))
         pygame.draw.rect(win, (0,0,0), (320, 0, 640, 150),4)
         pygame.draw.rect(win, (0,0,0), (320, 570, 640, (720-570)),4)
+        pygame.draw.rect(win, (99, 84, 60), (240, 220, 800, 280))
+        pygame.draw.rect(win, (0, 0, 0), (240, 220, 800, 280), 4)
+
+        #decks on right side of screen
+        pygame.draw.rect(win, (0,0,0), (1115, 440, 91, 123), 4)
+        win.blit(playerOneCardBackImage, (1115, 440))
+        pygame.draw.rect(win, (0, 0, 0), (1115, 190-33, 91, 123), 4)
+        win.blit(playerOneCardBackImage, (1115, 190-33))
+
+
+      #  win.blit("End Turn", 1105, 330)
+
+        win.blit(playerOneCardBackImage, (350, 20))
+
         
 
 def redrawWindow(win,Player,Card,boardLook,Player2):
-    win.blit(background,(0,0))
+    win.blit(background, (0, 0))
+    boardLook.draw(win, turnCount)
     Player.draw(win)
     Player2.draw(win)
     Card.draw(win)
-    boardLook.draw(win)
     pygame.display.update()
 
 
 def main():
     n = Network()
     startPos = read_pos(n.getPos())
-    p = Player(startPos[0], startPos[1], 100, 100, (0, 255, 0))
-    p2 = Player(0, 0, 100, 100, (0,255, 0))
+    p = Player(startPos[0], startPos[1], 100, 100, (0, 255, 0), 1)
+    p2 = Player(0, 0, 100, 100, (0,255, 0), 0)
 
     c = Card(100, 100, 50, 100, 1, 1, 1, (0,255,0))
     d = boardLook(500, 600, 200, 400, 1)
@@ -126,7 +176,6 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
-
         p.move()
        # win.blit(background, (0,0))
         redrawWindow(win,p,c,d,p2)
